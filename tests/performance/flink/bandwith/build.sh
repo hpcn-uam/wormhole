9b5c47d042accdc813d60e5c3a5442bcf0041b78
root <root@nrg.ii.uam.es>
2016-03-09 17:20:15 +0000

    Flink test

--- tests/performance/flink/bandwith/build.sh
+++ tests/performance/flink/bandwith/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-javac -cp /usr/hdp/2.4.0.0-169/storm/lib/storm-core-0.10.0.2.4.0.0-169.jar storm/bw/*java && jar cf bw.jar storm/
+LIBS=/root/flink/build-target/lib/flink-dist_2.10-1.1-SNAPSHOT.jar
+SRC=org/apache/flink/streaming/examples/socket
+FOLDER=org
+
+
+rm -rf $SRC/*class
+javac -cp $LIBS $SRC/*java && jar cvfm bw.jar META-INF/manifest.txt $FOLDER
+
+gcc -O3 -Wall gentext.c -o gentext -std=gnu11
