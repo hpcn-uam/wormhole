27dbd764ab61943da56b90367d4d11ab2668ee36
Rafael Leira Osuna <rafael.leira@uam.es>
2016-03-29 13:00:23 +0200

    JavaApi allows runscript-parameters

--- src/langApis/Java/es_hpcn_wormhole_Einstein.cpp
+++ src/langApis/Java/es_hpcn_wormhole_Einstein.cpp
@@ -15,7 +15,19 @@ JNIEXPORT jint JNICALL Java_es_hpcn_wormhole_Einstein_init
 	const char *c_configFileName = env->GetStringUTFChars(configFileName, NULL);
 	const char *c_listenIp       = env->GetStringUTFChars(listenIp, NULL);
 
-	_JWH_einstein = new Einstein(c_configFileName, c_listenIp, listenPort, autoDeployWorms == JNI_TRUE);
+	vector<string> runparams;
+
+	if (env->GetArrayLength(RunParams) > 0) {
+		for (int i = 0; i < env->GetArrayLength(RunParams); i++) {
+			jobject obj = env->GetObjectArrayElement(RunParams, i);
+			jstring str = (jstring) obj;
+			const char *strchars = env->GetStringUTFChars(str, NULL);
+			runparams.push_back(string(strchars));
+			env->ReleaseStringUTFChars(str, strchars);
+		}
+	}
+
+	_JWH_einstein = new Einstein(c_configFileName, c_listenIp, listenPort, autoDeployWorms == JNI_TRUE, runparams);
 
 	env->ReleaseStringUTFChars(configFileName, c_configFileName);
 	env->ReleaseStringUTFChars(configFileName, c_listenIp);
