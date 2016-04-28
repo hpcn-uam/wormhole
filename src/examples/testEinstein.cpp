fc7d3de6b177c9ef71b61ec1fa1db32d763d88e9
Rafael Leira Osuna <rafael.leira@uam.es>
2016-04-28 16:57:49 +0200

    einstein app now supports a 4º paremeter: the script parameter

--- src/examples/testEinstein.cpp
+++ src/examples/testEinstein.cpp
@@ -11,6 +11,7 @@ class EinsteinTester: public Einstein
  public:
 	EinsteinTester();
 	EinsteinTester(string configFilePath, string einsteinIp, bool autoDeployWorms);
+	EinsteinTester(string configFilePath, string einsteinIp, bool autoDeployWorms, vector<string> runParams);
 	~EinsteinTester();
 };
 
@@ -21,8 +22,6 @@ EinsteinTester::EinsteinTester() : EinsteinTester("../src/examples/test.conf", "
 
 EinsteinTester::EinsteinTester(string configFilePath, string einsteinIp, bool autoDeployWorms) : Einstein(configFilePath, einsteinIp, 5000, autoDeployWorms)
 {
-
-
 	assert(ec.listenIp == inet_addr("150.244.58.77"));
 	assert(ec.listenPort == 5000);
 
@@ -32,20 +31,42 @@ EinsteinTester::EinsteinTester(string configFilePath, string einsteinIp, bool au
 	assert(ec.connections.at(1)->ws.IP == inet_addr("150.244.58.77"));
 	assert(ec.connections.at(1)->ws.connectionDescriptionLength == 29);
 	assert(!memcmp(ec.connections.at(1)->ws.connectionDescription, "1", 29));
-
 }
+
+EinsteinTester::EinsteinTester(string configFilePath, string einsteinIp, bool autoDeployWorms, vector<string> runParams) : Einstein(configFilePath, einsteinIp, 5000, autoDeployWorms, runParams)
+{
+	assert(ec.listenIp == inet_addr("150.244.58.77"));
+	assert(ec.listenPort == 5000);
+
+	assert(ec.connections.at(1)->ws.id == 1);
+	assert(ec.connections.at(1)->ws.listenPort == 10001);
+	assert(ec.connections.at(1)->ws.core == -1);
+	assert(ec.connections.at(1)->ws.IP == inet_addr("150.244.58.77"));
+	assert(ec.connections.at(1)->ws.connectionDescriptionLength == 29);
+	assert(!memcmp(ec.connections.at(1)->ws.connectionDescription, "1", 29));
+}
+
 EinsteinTester::~EinsteinTester()
 {
 
 }
-
-
 
 int main(int argc, char **argv)
 {
 	if (argc != 3) {
 		try {
 			EinsteinTester einsTester;
+			std::cout << "Éxito\n";
+
+		} catch (exception &e) {
+			std::cerr << "Excepción: " << e.what() << '\n';
+		}
+
+	} else if (argc == 4) {
+		try {
+			vector<string> runParams;
+			runParams.push_back(argv[3]);
+			EinsteinTester einsTester(argv[1], argv[2], true, runParams);
 			std::cout << "Éxito\n";
 
 		} catch (exception &e) {
