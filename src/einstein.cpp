59b208a0c5069201203ef71bdb5a1fc550c48e6b
Rafael Leira Osuna <rafael.leira@uam.es>
2016-05-03 20:57:32 +0200

    changed scp to rsync in order to optimize tests

--- src/einstein.cpp
+++ src/einstein.cpp
@@ -385,7 +385,7 @@ void EinsConn::deployWorm(Eins2WormConn &wc)
 	string executable = "";
 
 	if (copyData) {
-		executable = "scp " + wc.programName + ".tgz " + wc.host + ":~";
+		executable = "rsync -au " + wc.programName + ".tgz " + wc.host + ":~";
 
 		if (system(executable.c_str())) {
 			cerr << "error executing comand: \"" << executable << "\"" << endl;
