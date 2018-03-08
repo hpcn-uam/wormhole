646909c57f46d774c94cb448a3481c459d247721
Rafael Leira Osuna <rafael.leira@uam.es>
2018-03-08 17:32:51 +0100

    detectpro.sh -- multi instancia

--- src/examples/runscripts/detectPro.sh
+++ src/examples/runscripts/detectPro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTFILE=$(echo $$)
+OUTFILE=$(echo detectPro $WORM_ID)
 
 ulimit -c unlimited
 FILES="~/.bashrc ~/.profile /etc/profile"
@@ -16,11 +16,16 @@ done
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib
 echo "LIBPATH = " $LD_LIBRARY_PATH &>> /tmp/detectPro.$OUTFILE.out
 
-mkdir -p /root/detectPro/extra/data
-mkdir -p /root/detectPro/extra/capture
-mkdir -p /root/detectPro/extra/flows
+mkdir -p /root/detectPro/instance/$WORM_ID/data
+mkdir -p /root/detectPro/instance/$WORM_ID/capture
+mkdir -p /root/detectPro/instance/$WORM_ID/flows
+mkdir -p /root/detectPro/instance/$WORM_ID/cfg
+
+
+cp /root/detectPro/extra/cfg/{alarms.cfg,networks.cfg,ordenMenuRedes.cfg,supernetworks.cfg /root/detectPro/instance/$WORM_ID/cfg/.
+sed -e 's/detectPro\/extra/detectPro\/instance\/'$WORM_ID'/g' instance/cfg/global.cfg > /root/detectPro/instance/$WORM_ID/cfg/global.cfg
 
 date &> /tmp/detectPro.time
 #valgrind --vgdb=yes --vgdb-error=0 --track-origins=yes \
-./detectPro extra/cfg/global.cfg &>> /tmp/detectPro.$OUTFILE.out
+./detectPro /root/detectPro/instance/$WORM_ID/cfg/global.cfg &>> /tmp/detectPro.$OUTFILE.out
 date &>> /tmp/detectPro.time
