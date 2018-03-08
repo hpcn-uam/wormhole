8da432debc36c9b331247954068f00887f8c28e6
Rafael Leira Osuna <rafael.leira@uam.es>
2018-03-08 19:32:25 +0100

    detectpro.sh

--- src/examples/runscripts/detectPro.sh
+++ src/examples/runscripts/detectPro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTFILE=$(echo detectPro $WORM_ID)
+OUTFILE=$(echo $WORM_ID)
 
 ulimit -c unlimited
 FILES="~/.bashrc ~/.profile /etc/profile"
@@ -23,7 +23,7 @@ mkdir -p /root/detectPro/instance/$WORM_ID/cfg
 
 
 cp /root/detectPro/extra/cfg/{alarms.cfg,networks.cfg,ordenMenuRedes.cfg,supernetworks.cfg /root/detectPro/instance/$WORM_ID/cfg/.
-sed -e 's/detectPro\/extra/detectPro\/instance\/'$WORM_ID'/g' instance/cfg/global.cfg > /root/detectPro/instance/$WORM_ID/cfg/global.cfg
+sed -e 's/detectPro\/extra/detectPro\/instance\/'$WORM_ID'/g' extra/cfg/global.cfg > /root/detectPro/instance/$WORM_ID/cfg/global.cfg
 
 date &> /tmp/detectPro.time
 #valgrind --vgdb=yes --vgdb-error=0 --track-origins=yes \
